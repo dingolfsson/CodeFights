dd54a1e64f5c29fb7e1edd9c4e7e35a666256462
ingolfsson <daniel.ingolfsson@gmail.com>
2017-04-05 13:38:47 +0000

    Update

--- Challenges/consecutiveBits.js
+++ Challenges/consecutiveBits.js
@@ -1,5 +1,10 @@
+consecutiveBit = n => (/1{2,}/).test(n.toString(2))
+
+
+/*
 consecutiveBit = n =>
     n.toString(2)
-    .split``.map((v,i,a) =>
-    v == "1" && a[i+1] == "1" ? 1 : 0)
+    .split``
+    .map((v,i,a) => v == "1" && a[i+1] == "1" ? 1 : 0)
     .includes(1)
+    */
--- Challenges/removeDigits.js
+++ Challenges/removeDigits.js
@@ -1,11 +1,37 @@
-a = [1e15, 0]
+y = i = 0, x = 9e9
 removeDigits = (n, k) => {
-    n = (''+n).split``
-    for (i = 0; i <= n.length-k; i++) {
-        t = "";
-        for (j = i; j < k+i; j++) t += n[j]
-        a[0] = a[0] > +t ? +t : a[0]
-        a[1] = a[1] < +t ? +t : a[1]
+    n = ''+n
+    while (i <= n.length - k) {
+        t = +n.slice(i, k+i++)
+        t > y ? y = t : 0
+        t < x ? x = t : 0
+    }
+    return [x, y]
+}
+
+
+/*
+a = [9e9, 0]
+removeDigits = (n, k) => {
+    n = ''+n
+    for (i=0; i<=n.length-k;i++) {
+        t = +n.slice(i,i+k)
+        a[0] = a[0] > t ? t : a[0]
+        a[1] = a[1] < t ? t : a[1]
     }
     return a
 }
+*/
+
+
+/*
+t = '', x = 9e9, y = 0
+m=Math
+removeDigits = (n, k) => 
+    [...n=""+n].map((e, i) => i <= n.length-k && 
+        [x = m.min(x,n.slice(i,i+k))
+        ,y = m.max(y,n.slice(i,i+k)) ]
+        
+    )
+    && [x,y]
+*/
